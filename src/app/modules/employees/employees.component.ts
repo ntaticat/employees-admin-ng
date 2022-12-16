08807547b91039e3719ae4e5995f5ee9ef5f0613
Rafael Estrada <ntaticat@gmail.com>
2022-12-15 23:59:17 -0600

    update employees table

--- src/app/modules/employees/employees.component.ts
+++ src/app/modules/employees/employees.component.ts
@@ -1,10 +1,49 @@
 import { Component } from '@angular/core';
+import { MatTableDataSource } from '@angular/material/table';
+import {
+  Observable,
+  Observer,
+  PartialObserver,
+  Subject,
+  Subscription,
+} from 'rxjs';
+
+export interface IEmployee {
+  id: number;
+  name: string;
+  middleName: string;
+  surName: string;
+}
+
+const EMPLOYEE_DATA: IEmployee[] = [
+  { id: 1, name: 'Rafael', middleName: 'Estrada', surName: 'Piñon' },
+  { id: 2, name: 'Rafael', middleName: 'Estrada', surName: 'Piñon' },
+  { id: 3, name: 'Rafael', middleName: 'Estrada', surName: 'Piñon' },
+  { id: 4, name: 'Rafael', middleName: 'Estrada', surName: 'Piñon' },
+  { id: 5, name: 'Rafael', middleName: 'Estrada', surName: 'Piñon' },
+  { id: 6, name: 'Rafael', middleName: 'Estrada', surName: 'Piñon' },
+  { id: 7, name: 'Rafael', middleName: 'Estrada', surName: 'Piñon' },
+];
 
 @Component({
   selector: 'app-employees',
   templateUrl: './employees.component.html',
-  styleUrls: ['./employees.component.scss']
+  styleUrls: ['./employees.component.scss'],
 })
 export class EmployeesComponent {
+  employeeTableColumns: string[] = [
+    'ID',
+    'Nombre',
+    'Apellido Paterno',
+    'Apellido Materno',
+    'Acciones',
+  ];
+  employeeDataSource = new MatTableDataSource(EMPLOYEE_DATA);
 
+  constructor() {}
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.employeeDataSource.filter = filterValue.trim().toLowerCase();
+  }
 }
